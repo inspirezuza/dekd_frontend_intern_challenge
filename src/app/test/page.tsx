e29a8ab48e2f73b088248bf39c64d3c39e15af70
inspirezuza <inspire7220@gmail.com>
2024-02-27 17:22:57 +0700

    shadcnui carousel

---
+++ src/app/test/page.tsx
@@ -0,0 +1,11 @@
+import { CarouselPlugin } from "@/components/component/CarouselPlugin";
+
+export default function Test() {
+  return (
+    <>
+      <div className="max-w-md mx-auto">
+        <CarouselPlugin />
+      </div>
+    </>
+  );
+}
---
+++ src/components/component/CarouselPlugin.tsx
@@ -0,0 +1,44 @@
+"use client";
+import * as React from "react";
+import Autoplay from "embla-carousel-autoplay";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export function CarouselPlugin() {
+  const plugin = React.useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: true })
+  );
+
+  return (
+    <Carousel
+      plugins={[plugin.current]}
+      className="w-full max-w-xs"
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
+      opts={{ align: "center", loop: true }}
+    >
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className="basis-1/2">
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
